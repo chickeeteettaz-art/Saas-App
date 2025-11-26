b36e901e3c91802af9304557573d494d852c6f3f
Chicketeetta Zinhle <chickeeteettaz@gmail.com>
2025-11-26 09:45:30 +0200

    Added the navbar feature

---
+++ components/NavItems.tsx
@@ -0,0 +1,29 @@
+'use client';
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+import {cn} from "@/lib/utils";
+
+const navItems = [
+    {label:'Home', href:'/'},
+    {label:'My Journey', href:'/my-journey'},
+    {label:'Companions', href:'/companions'}
+]
+
+const NavItems = () => {
+
+    const pathName = usePathname()
+
+    return (
+
+        <nav className='flex items-center gap-4'>
+            {navItems.map(({label, href})=>
+                (
+                    <Link href={href} key={label} className={cn(pathName === href && 'text-primary font-semibold')}>
+                        {label}
+                    </Link>
+                )
+            )}
+        </nav>
+    )
+}
+export default NavItems;
