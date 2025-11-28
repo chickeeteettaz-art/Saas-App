57af330a6b6e4a1500c4cdefb2376ea6be0669a0
Chicketeetta Zinhle <chickeeteettaz@gmail.com>
2025-11-28 20:40:12 +0200

    search feature implementation

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+'use client'
+
+import { usePathname, useSearchParams ,useRouter} from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import Image  from 'next/image'
+import { formUrlQuery, removeKeysFromUrlQuery } from '@jsmastery/utils'
 
 const SearchInput = () => {
-  return (
-    <div>SearchInput</div>
-  )
+
+    const pathName = usePathname()
+    const router = useRouter()
+    const searchParams = useSearchParams()
+
+    const query = searchParams.get('topic') ||''
+    const [searchQuery,setSearchQueary] = useState('')
+
+    useEffect(() =>{
+
+        if(searchQuery){
+            const newUrl = formUrlQuery({
+            params: searchParams.toString(),
+            key: "topic",
+            value: searchQuery,
+            });
+
+            router.push(newUrl,{scroll:false});
+        }else{
+            if(pathName === '/companions'){
+                const newUrl = removeKeysFromUrlQuery({
+                    params: searchParams.toString(),
+                    keysToRemove: ['topic']
+                })
+                router.push(newUrl,{scroll:false})
+            }
+        }
+    },[searchQuery,router,searchParams,pathName])
+    return (
+        <div className='relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit'>
+            <Image src={'/icons/search.svg'} alt='search' width={15} height={15}/>
+
+            <input placeholder='Search Companions'
+                className='outline-none'
+                value={searchQuery}
+                onChange={(e) => setSearchQueary(e.target.value)}
+            />
+        </div>
+    )
 }
 
 export default SearchInput
